96e5ba0518a44a7e6f4dd49e0ae0916bc6abdeb4
Filip Vagner <filip.vagner@orbit.cz>
2026-01-30 19:39:23 +0100

    added subnets output

--- modules/virtual-network/outputs.tf
+++ modules/virtual-network/outputs.tf
@@ -11,3 +11,13 @@ output "virtual_network_resource_ids" {
     for k, v in module.virtual_networks : k => v.resource_id
   }
 }
+
+output "subnets" {
+  description = <<DESCRIPTION
+Information about the subnets created in the module.
+
+Please refer to the subnet module documentation for details of the outputs
+DESCRIPTION
+  # All subnets now use the subnet module for consistent interface
+  value = module.subnet
+}
--- outputs.tf
+++ outputs.tf
@@ -72,3 +72,13 @@ output "virtual_network_resource_ids" {
   description = "A map of virtual network resource ids, keyed by the var.virtual_networks input map. Only populated if the virtualnetwork submodule is enabled."
   value       = local.virtual_network_resource_ids
 }
+
+output "subnets" {
+  description = <<DESCRIPTION
+Information about the subnets created in the module.
+
+Please refer to the subnet module documentation for details of the outputs
+DESCRIPTION
+  # All subnets now use the subnet module for consistent interface
+  value = module.subnet
+}
