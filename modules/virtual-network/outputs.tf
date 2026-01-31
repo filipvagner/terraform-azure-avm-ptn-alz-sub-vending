5b6e7b06ddc1662545ebf2c13d28b7a48e4d0c8b
Filip Vagner <filip.vagner@orbit.cz>
2026-01-31 14:32:03 +0100

    Unified output values

--- modules/virtual-network/outputs.tf
+++ modules/virtual-network/outputs.tf
@@ -15,14 +15,14 @@ output "virtual_network_resource_ids" {
 output "virtual_network_resource_info" {
   value = {
     for k, v in module.virtual_networks : k => {
-      vnet_id       = v.resource_id
-      vnet_name     = v.name
-      address_space = v.address_spaces
+      vnet_id            = v.resource_id
+      vnet_name          = v.name
+      vnet_address_space = v.address_spaces
       subnets = {
         for k_s, v_s in v.subnets : k_s => {
-          subnet_id        = v_s.resource_id
-          name             = v_s.name
-          address_prefixes = v_s.address_prefixes
+          subnet_id               = v_s.resource_id
+          subnet_name             = v_s.name
+          subnet_address_prefixes = v_s.address_prefixes
         }
       }
     }
