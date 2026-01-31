b3b932e5c8c23cb3aaac0c8f3026e53d931a23ee
Filip Vagner <filip.vagner@orbit.cz>
2026-01-31 11:00:31 +0100

    Added vnets info to output

--- modules/virtual-network/outputs.tf
+++ modules/virtual-network/outputs.tf
@@ -12,12 +12,19 @@ output "virtual_network_resource_ids" {
   }
 }
 
-# output "subnets" {
-#   description = <<DESCRIPTION
-# Information about the subnets created in the module.
-
-# Please refer to the subnet module documentation for details of the outputs
-# DESCRIPTION
-#   # All subnets now use the subnet module for consistent interface
-#   value = module.subnet
-# }
+output "virtual_network_resource_info" {
+  value = {
+    for k, v in module.virtual_networks : k => {
+      vnet_id       = v.resource_id
+      vnet_name     = v.name
+      address_space = v.address_spaces
+      subnets = {
+        for k_s, v_s in v.subnets : k_s => {
+          subnet_id        = v_s.resource_id
+          name             = v_s.name
+          address_prefixes = v_s.address_prefixes
+        }
+      }
+    }
+  }
+}
